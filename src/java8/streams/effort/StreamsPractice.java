62d716bf455146a404848c668589d229616af475
aniket <iamastronomeraniket@gmail.com>
2020-03-15 15:48:44 +0530

    //sorting bubble

--- src/java8/streams/effort/StreamsPractice.java
+++ src/java8/streams/effort/StreamsPractice.java
@@ -1,13 +1,40 @@
 package java8.streams.effort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class StreamsPractice {
 
 	public static void main(String[] args) {
 		
-		//Bubble Sort an given array using Streams.
+		/*
+		 * Simplest of the Sorting algo. 
+		 * 
+		 */
 		
 		
+		int[] arr  = {2,5,6,8,1,2,3,45,56,4,1,2,3,9,8,4};
 		
+		boolean swapped;
+		
+		do {
+			swapped = false;
+			for(int i=0;i<arr.length-1;i++) {
+				if(arr[i]>arr[i+1]) {
+					int temp = arr[i+1];
+					arr[i+1] = arr[i];
+					arr[i] = temp;
+					//Indicate Swapping occurs
+					swapped = true;
+				}
+			}
+		}while(swapped);//till swapping
+		
+		for(int x : arr) {
+			System.out.print(x+" ");
+		}
 	}
 
 }
